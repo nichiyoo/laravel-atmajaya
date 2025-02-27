f352440e93d5393969ace9bc010a98bad55e12e4
nichiyoo <61191577+nichiyoo@users.noreply.github.com>
2025-02-27 15:55:50 +0700

    fix: change nodejs version to 18 for vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     $middleware->alias([
       'role' => RoleMiddleware::class
     ]);
+    $middleware->trustProxies(at: '*');
   })
   ->withExceptions(function (Exceptions $exceptions) {
     //
